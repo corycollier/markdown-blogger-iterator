aad1347f15970df0e0763b0e7f9d3d1a0219c36c
Cory Collier <cory.collier@ahss.org>
2017-05-05 11:53:09 -0400

    Implementing the PluginInterface

---
+++ src/IteratorPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MarkdownBlogger\Plugin;
+
+use MarkdownBlogger\PluginInterface
+
+class IteratorPlugin implements PluginInterface
+{
+    public function __construct()
+    {
+        echo 'got here';
+    }
+}
